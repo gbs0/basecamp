0604501f0e9aa1723edeefbcc9cc43e838e3e581
Gabriel Schiavo <gabriel.penha@agrotools.com.br>
2022-03-30 16:10:44 -0300

    Add simple C program

---
+++ c_basics/hello42.c
@@ -0,0 +1,19 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   hello42.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gschiavo <gschiavo@student.42sp.org.br>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/03/30 16:08:35 by gschiavo          #+#    #+#             */
+/*   Updated: 2022/03/30 16:10:02 by gschiavo         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+// A simple C program to print the answer to the ultimate question of life, the universe, and everything.
+#include <stdio.h>
+
+int main() {
+    printf("The answer to the ultimate question of life, the universe, and everything is %d.\n", 42);
+    return 0;
+}
