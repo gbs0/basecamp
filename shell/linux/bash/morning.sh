27b498b2fedf501f5a31777519d19d189ba19990
Gabriel Schiavo <gabriel.penha@agrotools.com.br>
2022-03-31 21:03:38 -0300

    fix workspace'

--- shell/linux/bash/morning.sh
+++ shell/linux/bash/morning.sh
@@ -3,7 +3,7 @@
 ME=`whoami`
 echo "Good morning, $ME!"
 echo "Today is $(date)."
-echo "You currently have $((`free -m | grep Mem | awk '{print $2}')`/1024) GB of RAM."
-echo "You currently have $(`df -h | grep /dev/sda1 | awk '{print $4}'`) available in disk space."
+echo "You currently have $((`free -m | grep Mem | awk '{print $2}')`/1024)) GB of RAM."
+echo "You currently have $(`df -h | grep /10.51.1.1:/mnt/storage/k8s/student-workspace-gschiavo-workspace-pvc-c4b11092-3a23-4f05-8452-6a233fd9ca7f | awk '{print $4}'`) available in disk space."
 echo "Your current directory is $(pwd)."
 
