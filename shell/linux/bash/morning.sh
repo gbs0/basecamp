5ba22f41d710dce1dd6229b770c2dedac5c2d66e
Gabriel Schiavo <gabriel.penha@agrotools.com.br>
2022-03-31 22:08:07 -0300

    fix syntax

--- shell/linux/bash/morning.sh
+++ shell/linux/bash/morning.sh
@@ -5,7 +5,7 @@ MEMORY_FREE=`free -m | grep Mem | awk '{print $4}'`
 DISK_SPACE=`df -h | grep 10.51.1.1:/mnt/storage/k8s/student-workspace-gschiavo-workspace-pvc-c4b11092-3a23-4f05-8452-6a233fd9ca7f | awk '{print $4}'`
 echo "Good morning, $ME!"
 echo "Today is $(date)."
-echo "You currently have $MEMORY_FREE MB of RAM."
-echo "You currently have $(DISK_SPACE) available in disk space."
-echo "Your current directory is $(pwd)."
+echo "Available RAM | $MEMORY_FREE MB"
+echo "   | $DISK_SPACE available in disk space."
+echo "Directory is $(pwd)."
 
