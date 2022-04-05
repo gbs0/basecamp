4a03d5f62ce757da0dfb2ca4154c5cb22ebde5db
Gabriel Schiavo <gabrielschiavo0@gmail.com>
2022-04-05 19:29:26 -0300

    Fix ex02 timestamp files

--- projects/00-Shell/ex02/files.sh
+++ projects/00-Shell/ex02/files.sh
@@ -16,7 +16,7 @@
 for i in $(seq 0 6)
 do
   `rm -rf test${i}`
-  if [ `touch -m -t 201906020730.00 test${i}` ]; then
+  if [ `touch -m -t 202106010730.00 test${i}` ]; then
   	echo "Error: Cannot create Files"
   else
 	echo "File $i was succesfully created"
@@ -28,7 +28,7 @@ FILE_ZERO=./test0
 RMV_FILE_ZERO=`rm ./test0`
 CRT_DIR_ZERO=`mkdir test0`
 CHN_PERMISSION_ZERO=`chmod 715 ./test0`
-CHN_TIMESTAMP_ZERO=`touch -h -t 202106020730.00 test0`
+CHN_TIMESTAMP_ZERO=`touch -h -t 202106010730.00 test0`
 if [ -d "$FILE_ZERO" ]; then
 	echo "-> Removing file in ${FILE_ZERO}..."
 	$RMV_FILE_ZERO
@@ -43,7 +43,7 @@ fi
 FILE_ONE=./test1
 ADD_CHAR_ONE=`echo "042" >> ./test1`
 CHN_PERMISSION_ONE=`chmod 714 $FILE_ONE`
-CHN_TIMESTAMP_ONE=`touch -t 202106020730.00 $FILE_ONE`
+CHN_TIMESTAMP_ONE=`touch -t 202106010730.00 $FILE_ONE`
 if [ -f "$FILE_ONE" ]; then
 	echo "-> Working in $FILE_ONE"
 	echo "-> Changing Permission..."
@@ -56,7 +56,7 @@ FILE_TWO=./test2
 RMV_FILE_TWO=`rm ./test2`
 CRT_DIR_TWO=`mkdir test2`
 CHN_PERMISSION_TWO=`chmod 504 $FILE_TWO`
-CHN_TIMESTAMP_TWO=`touch -t 202106020730.00 test2`
+CHN_TIMESTAMP_TWO=`touch -t 202106010730.00 test2`
 if [ -d "$FILE_TWO" ]; then
 	echo "-> Removing file in ${FILE_TWO}..."
 	$RMV_FILE_TWO
@@ -84,7 +84,7 @@ FILE_THREE=./test3
 ADD_CHAR_THREE=`echo "" >> $FILE_THREE`
 CHN_PERMISSION_THREE=`chmod 404 $FILE_THREE`
 CRT_HARDLINK_THREE=`ln ./test3 ./test5`
-CHN_TIMESTAMP_THREE=`touch -h -t 202106020730.00 $FILE_THREE`
+CHN_TIMESTAMP_THREE=`touch -h -t 202106010730.00 $FILE_THREE`
 if [ -f "$FILE_THREE" ]; then
 	echo "-> Working in $FILE_THREE"
 	echo "-> Add Chars to $FILE_THREE..."
@@ -100,7 +100,7 @@ fi
 FILE_FOUR=./test4
 ADD_CHAR_FOUR=`echo "a" >> $FILE_FOUR`
 CHN_PERMISSION_FOUR=`chmod 641 $FILE_FOUR`
-CHN_TIMESTAMP_FOUR=`touch -t 202106020730.00 $FILE_FOUR`
+CHN_TIMESTAMP_FOUR=`touch -t 202106010730.00 $FILE_FOUR`
 if [ -f "$FILE_FOUR" ]; then
 	echo "-> Working in $FILE_FOUR"
 	echo "-> Add Chars to $FILE_FOUR..."
@@ -118,7 +118,7 @@ fi
 FILE_SIX=./test6
 RMV_FILE_SIX=`rm ./test6`
 SYM_FILE_SIX=`ln -sf test0 ./test6`
-CHN_TIMESTAMP_SIX=`touch -h -t 202106020730.00 test6`
+CHN_TIMESTAMP_SIX=`touch -h -t 202106010730.00 test6`
 if [ -d "$FILE_SIX" ]; then
 	echo "-> Removing file in ${FILE_SIX}..."
 	$RMV_FILE_SIX
@@ -147,4 +147,5 @@ fi
 # Execute final shell operation
 echo "$(ls -l)"
 
-echo "$(tar -cf exo2.tar *)"
+echo "$(tar -cf exo2.tar test0 test1 test2 test3 test4 test5 test6)"
+echo "File exo2.tar created!"
