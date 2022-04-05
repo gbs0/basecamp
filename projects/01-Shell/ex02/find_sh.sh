dceefa5756e2b03b46b40ef5b87f053bf8ef5eb7
Gabriel Schiavo <gabrielschiavo0@gmail.com>
2022-04-05 19:30:47 -0300

    WIP in ex02

---
+++ projects/01-Shell/ex02/find_sh.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+Files=(`find .  -type f -name "*.sh"`)
+Files_number = ${#Files[@]}
+
+for ((i = 0 ; i <= $Files_number ; i++)); do
+  echo `${Files[$i]} | awk '{ gsub("./", ""); gsub(/^.*\.(sh)$/, ""); system( "echo "  $0) }'`
+done
+
+
+# awk '{gsub(/\t/, ","); gsub(/,,/, ",-,"); gsub(/,,/, ",-,")
+echo "./ex02/find_sh.sh" | awk '{gsub(/[^\/]+/\.\./, ","); gsub(/\./, ""); gsub(/.sh/, ",-,");  system( "echo "  $0) }'
