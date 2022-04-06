1865a554cbc35214aacda70d74732765065311ca
Gabriel Schiavo <gabrielschiavo0@gmail.com>
2022-04-06 11:34:51 -0300

    WIP ex02/

--- projects/01-Shell/ex02/find_sh.sh
+++ projects/01-Shell/ex02/find_sh.sh
@@ -1,14 +1,19 @@
 #!/usr/bin/env bash
 
-Files=(`find .  -type f -name "*.sh"`)
-Files_number = ${#Files[@]}
 
-for ((i = 0 ; i <= $Files_number ; i++)); do
-  echo `${Files[$i]} | awk '{ gsub("./", ""); gsub(/^.*\.(sh)$/, ""); system( "echo "  $0) }'`
-done
+
+
+# Files=(`find .  -type f -name "*.sh"`)
+
+# for ((i = 0 ; i <= ${#Files[@]} ; i++)); do
+#   #echo `${Files[$i]} | awk '{ gsub("./", ""); gsub(/^.*\.(sh)$/, ""); system( "echo "  $0) }'`
+#   echo `${Files[$i]} | sed -e 's/\.sh$//' | awk '{ gsub("./", ""); system( "echo "  $0) }'`
+# done
 
 
 # awk '{gsub(/\t/, ","); gsub(/,,/, ",-,"); gsub(/,,/, ",-,")
-echo "./ex02/find_sh.sh" | awk '{gsub(/[^\/]+/\.\./, ","); gsub(/\./, ""); gsub(/.sh/, ",-,");  system( "echo "  $0) }'
+# echo "./ex02/find_sh.sh" | awk '{gsub(/[^\/]+/\.\./, ","); gsub(/\./, ""); gsub(/.sh/, ",-,");  system( "echo "  $0) }'
 
-ls  -1 | sed -e 's/\.sh$//'
+
+# echo $(`find .  -type f -name "*.sh" | sed -e 's/\.sh$//' | awk '{ gsub("./", ""); system( "echo "  $0) }'`)
+find .  -type f -name "*.sh" | sed -e 's/\.sh$//'  | awk '{ gsub("./", ""); system( "echo "  $0) }'
