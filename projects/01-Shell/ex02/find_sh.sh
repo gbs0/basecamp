2e33b30fb6cb96d4e53b79d2bc3452c15c27b1f5
Gabriel Schiavo <gschiavo@student.42sp.org.br>
2022-04-06 16:50:29 +0200

    Finished ex02/

--- projects/01-Shell/ex02/find_sh.sh
+++ projects/01-Shell/ex02/find_sh.sh
@@ -1,8 +1,5 @@
 #!/usr/bin/env bash
 
-
-
-
 # Files=(`find .  -type f -name "*.sh"`)
 
 # for ((i = 0 ; i <= ${#Files[@]} ; i++)); do
@@ -10,10 +7,7 @@
 #   echo `${Files[$i]} | sed -e 's/\.sh$//' | awk '{ gsub("./", ""); system( "echo "  $0) }'`
 # done
 
-
 # awk '{gsub(/\t/, ","); gsub(/,,/, ",-,"); gsub(/,,/, ",-,")
 # echo "./ex02/find_sh.sh" | awk '{gsub(/[^\/]+/\.\./, ","); gsub(/\./, ""); gsub(/.sh/, ",-,");  system( "echo "  $0) }'
 
-
-# echo $(`find .  -type f -name "*.sh" | sed -e 's/\.sh$//' | awk '{ gsub("./", ""); system( "echo "  $0) }'`)
 find .  -type f -name "*.sh" | sed -e 's/\.sh$//'  | awk '{ gsub("./", ""); system( "echo "  $0) }'
