7f8c8a5c725490a69cb7241702cc7881b1dba7f5
Gabriel Schiavo <gabrielschiavo0@gmail.com>
2022-04-07 12:03:37 -0300

    Fixing Shell01 ex01 and ex05

--- projects/01-Shell/ex01/print_groups.sh
+++ projects/01-Shell/ex01/print_groups.sh
@@ -1,7 +1,3 @@
 #!/usr/bin/env bash
 
-
-Users=(`ls -pmu | groups $FT_USER | tr ' ' ','`)
-for ((i = 0 ; i <= ${#Users[@]} ; i++)); do
-  printf "${Users[$i]}"
-done
+groups $FT_USER | tr ' ' ',' | tr -d '\n'
--- projects/01-Shell/ex02/find_sh.sh
+++ projects/01-Shell/ex02/find_sh.sh
@@ -1,13 +1,3 @@
 #!/usr/bin/env bash
 
-# Files=(`find .  -type f -name "*.sh"`)
-
-# for ((i = 0 ; i <= ${#Files[@]} ; i++)); do
-#   #echo `${Files[$i]} | awk '{ gsub("./", ""); gsub(/^.*\.(sh)$/, ""); system( "echo "  $0) }'`
-#   echo `${Files[$i]} | sed -e 's/\.sh$//' | awk '{ gsub("./", ""); system( "echo "  $0) }'`
-# done
-
-# awk '{gsub(/\t/, ","); gsub(/,,/, ",-,"); gsub(/,,/, ",-,")
-# echo "./ex02/find_sh.sh" | awk '{gsub(/[^\/]+/\.\./, ","); gsub(/\./, ""); gsub(/.sh/, ",-,");  system( "echo "  $0) }'
-
 find .  -type f -name "*.sh" | sed -e 's/\.sh$//'  | awk '{ gsub("./", ""); system( "echo "  $0) }'
