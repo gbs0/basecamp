0728bb9edd2130d6d724168b6e0fe981200949ed
Gabriel Schiavo <gabrielschiavo0@gmail.com>
2022-04-07 14:35:14 -0300

    Finish ex03/

---
+++ projects/C00/ex03/ft_print_numbers.c
@@ -0,0 +1,13 @@
+// Header
+
+#include <unistd.h>
+
+void ft_print_numbers(void); {
+    int number;
+
+    number = 0;
+    while (number <= 99) {
+        write(1, &number, 1);
+        number++;
+    }
+}
