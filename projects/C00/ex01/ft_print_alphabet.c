407c22fd7b8f6b6273f811a7ef9fcfc5fda3d7fc
Gabriel Schiavo <gabrielschiavo0@gmail.com>
2022-04-07 14:16:38 -0300

    Compile ex01

--- projects/C00/ex01/ft_print_alphabet.c
+++ projects/C00/ex01/ft_print_alphabet.c
@@ -6,9 +6,22 @@
 /*   By: gschiavo <gschiavo@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/07 19:03:18 by gschiavo          #+#    #+#             */
-/*   Updated: 2022/04/07 19:03:59 by gschiavo         ###   ########.fr       */
+/*   Updated: 2022/04/07 14:11:34 by gschiavo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
 
+void ft_print_alphabet(void)
+{
+    char word;
+
+    word = 'a';
+    while (word <= 'z')
+    {
+        write(1, &word, 1);
+        word++;
+    }
+}
+
+int main(void) { };
