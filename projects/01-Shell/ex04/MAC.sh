73c3afc0403ee738407daed375a6d2b07ea6aa70
Gabriel Schiavo <gabrielschiavo0@gmail.com>
2022-04-09 10:23:17 -0300

    Fix ex04 blankspace

--- projects/01-Shell/ex04/MAC.sh
+++ projects/01-Shell/ex04/MAC.sh
@@ -1,1 +1,1 @@
-ifconfig -a |   grep txqueuelen  | grep ether | awk -F' ' '{print $2 "  " $3 " " $4 "  " $5}'
+ifconfig -a | grep txqueuelen | grep ether | awk -F' ' '{print $2 "  " $3 " " $4 "  " $5}'
