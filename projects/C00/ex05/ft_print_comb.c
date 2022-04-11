fad6ba21a5d8a6e9de1fa7b98f60e65fe8e2db5f
Gabriel Schiavo <gabrielschiavo0@gmail.com>
2022-04-11 17:47:53 -0300

    Finish C00/ex05

--- projects/C00/ex05/ft_print_comb.c
+++ projects/C00/ex05/ft_print_comb.c
@@ -6,66 +6,36 @@
 /*   By: gbs0 <gbs0@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/11 17:11:58 by gschiavo          #+#    #+#             */
-/*   Updated: 2022/04/11 15:22:44 by gbs0             ###   ########.fr       */
+/*   Updated: 2022/04/11 17:47:32 by gbs0             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
 
-void ft_putchar(char c)
+void	ft_print_comb(void)
 {
-    write(1, &c, 1);
-}
+	int	unidade;
+	int	dezena;
+	int	centena;
 
-void ft_print_dozens(char string[3])
-{
-    ft_putchar(string[0]);
-	ft_putchar(string[1]);
-	ft_putchar(string[2]);
-	if (!(string[0] == '7' && string[1] == '8' && string[2] == '9'))
+	unidade = '0' - 1;
+	while (++unidade <= '9')
 	{
-		ft_putchar(',');
-		ft_putchar(' ');
+		dezena = '0' - 1;
+		while (++dezena <= '9')
+		{
+			centena = '0' - 1;
+			while (++centena <= '9')
+			{
+				if (dezena > unidade && dezena < centena)
+				{
+					write (1, &unidade, 1);
+					write (1, &dezena, 1);
+					write (1, &centena, 1);
+					if (unidade != '7')
+						write (1, ", ", 2);
+				}
+			}
+		}
 	}
-
-}
-
-void ft_print_comb(void) 
-{
-    char string[3];
-
-    string[0] = '0';
-    string[1] = '1';
-    string[2] = '2';
-    
-    while(1)
-    {
-        while (string[2] <= '9')
-        {   
-            ft_print_dozens(string);
-            string[2]++;
-        }
-        
-        if (string[1] <= '8')
-        {
-            string[2] = string[1];
-            string[1]++;
-        }
-        else if (string[0] <= '7')
-        {
-            string[1] = string[0] + 1;
-            string[2] = string[1];
-        }
-        else
-            break;
-            
-    }
-        
-    
-}
-
-int main(void) {
-    ft_print_comb();
-    // ft_print_dozens();
-    return (0);
 }
