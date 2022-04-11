98b665ced68be443e7ac3122b8d10559e5c93494
Gabriel Schiavo <gabrielschiavo0@gmail.com>
2022-04-11 15:28:16 -0300

    WIP in C00/ex05

--- projects/C00/ex05/ft_print_comb.c
+++ projects/C00/ex05/ft_print_comb.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_print_comb.c                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: gschiavo <gschiavo@student.42sp.org.br>    +#+  +:+       +#+        */
+/*   By: gbs0 <gbs0@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/11 17:11:58 by gschiavo          #+#    #+#             */
-/*   Updated: 2022/04/11 17:12:58 by gschiavo         ###   ########.fr       */
+/*   Updated: 2022/04/11 15:22:44 by gbs0             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,15 +17,19 @@ void ft_putchar(char c)
     write(1, &c, 1);
 }
 
-void ft_print_dozens(void)
+void ft_print_dozens(char string[3])
 {
-    char i;
-    char j;
+    ft_putchar(string[0]);
+	ft_putchar(string[1]);
+	ft_putchar(string[2]);
+	if (!(string[0] == '7' && string[1] == '8' && string[2] == '9'))
+	{
+		ft_putchar(',');
+		ft_putchar(' ');
+	}
 
-    i = '0';
-    j = '0';
-    
 }
+
 void ft_print_comb(void) 
 {
     char string[3];
@@ -34,35 +38,34 @@ void ft_print_comb(void)
     string[1] = '1';
     string[2] = '2';
     
+    while(1)
+    {
         while (string[2] <= '9')
-        {
-            ft_putchar(string[0]);
-            ft_putchar(string[1]);
-            ft_putchar(string[2]);
-            if (string[0] != '7' || string[1] != '8' || string[2] != '9') {
-                ft_putchar(',');
-                ft_putchar(' ');
-            }
-            string[2]++; 
+        {   
+            ft_print_dozens(string);
+            string[2]++;
         }
         
-        while(string[1] <= '9')
+        if (string[1] <= '8')
         {
-            ft_putchar(string[0]);
-            ft_putchar(string[1]);
-            ft_putchar(string[2]);
-            if (string[0] != '7' || string[1] != '8' || string[2] != '9') {
-                ft_putchar(',');
-                ft_putchar(' ');
-            }
+            string[2] = string[1];
             string[1]++;
         }
+        else if (string[0] <= '7')
+        {
+            string[1] = string[0] + 1;
+            string[2] = string[1];
+        }
+        else
+            break;
+            
+    }
         
     
 }
 
-// int main(void) {
-//     // ft_print_comb();
-//     ft_print_dozens();
-//     return (0);
-// }
+int main(void) {
+    ft_print_comb();
+    // ft_print_dozens();
+    return (0);
+}
