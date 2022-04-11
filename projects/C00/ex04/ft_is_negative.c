67fb95622d4b51f513ba8bc79e0b603dff8958c5
Gabriel Schiavo <gschiavo@student.42sp.org.br>
2022-04-11 13:38:44 +0200

    Add Norminette good style for ex04

--- projects/C00/ex04/ft_is_negative.c
+++ projects/C00/ex04/ft_is_negative.c
@@ -6,18 +6,20 @@
 /*   By: gschiavo <gschiavo@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/07 19:51:55 by gschiavo          #+#    #+#             */
-/*   Updated: 2022/04/07 19:51:59 by gschiavo         ###   ########.fr       */
+/*   Updated: 2022/04/11 13:37:57 by gschiavo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-
 #include <unistd.h>
 
-void ft_is_negative(int n)
+void	ft_is_negative(int n)
 {
-    if (n < 0) {
-        write(1, "N", 1);
-    } else {
-        write(1, "P", 1);
-    };
+	if (n < 0)
+	{
+		write(1, "N", 1);
+	}
+	else
+	{
+		write(1, "P", 1);
+	}
 }
