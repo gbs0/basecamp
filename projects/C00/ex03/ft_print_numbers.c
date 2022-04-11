9c0004d71744dfec5fbbfe6660cc254458df3454
Gabriel Schiavo <gschiavo@student.42sp.org.br>
2022-04-11 13:35:02 +0200

    Add Norminette good style for ex03

--- projects/C00/ex03/ft_print_numbers.c
+++ projects/C00/ex03/ft_print_numbers.c
@@ -6,35 +6,13 @@
 /*   By: gschiavo <gschiavo@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/07 19:51:35 by gschiavo          #+#    #+#             */
-/*   Updated: 2022/04/08 16:59:58 by gschiavo         ###   ########.fr       */
+/*   Updated: 2022/04/11 13:34:13 by gschiavo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-
 #include <unistd.h>
 
-void ft_print_numbers(void) {
-    // int number;
-	// int limit;
-	int digit;
-
-	digit = 99;
-	// limit = 99;
-    // number = 0;
-	
-    // while (number <= limit) {
-    //     write(1, &number, 1);
-    //     number += 1;
-    // }
-
-	while (digit >= 0)
-	{
-		write(1, &digit, 1);
-		digit--;
-	}
-	
-}
-
-int main(void) {
-	ft_print_numbers();
+void	ft_print_numbers(void)
+{
+	write(1, "0123456789", 10);
 }
