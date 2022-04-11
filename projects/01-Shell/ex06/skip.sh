ac9374cd1ffb87740f6252ac36d48b94527dfd10
Gabriel Schiavo <gabrielschiavo0@gmail.com>
2022-04-11 12:09:46 -0300

    WIP on C00/ex07

--- projects/01-Shell/ex06/skip.sh
+++ projects/01-Shell/ex06/skip.sh
@@ -1,3 +1,4 @@
 #! /usr/bin bash
 
 ls -l | awk 'NR%2>0'
+
---
+++ projects/C00/ex05/ft_print_comb.c
@@ -0,0 +1,58 @@
+// Header
+
+#include <unistd.h>
+
+void ft_putchar(char c)
+{
+    write(1, &c, 1);
+}
+
+void ft_print_dozens(void)
+{
+    char i;
+    char j;
+
+    i = '0';
+    j = '0';
+    
+}
+void ft_print_comb(void) 
+{
+    char string[3];
+
+    string[0] = '0';
+    string[1] = '1';
+    string[2] = '2';
+    
+        while (string[2] <= '9')
+        {
+            ft_putchar(string[0]);
+            ft_putchar(string[1]);
+            ft_putchar(string[2]);
+            if (string[0] != '7' || string[1] != '8' || string[2] != '9') {
+                ft_putchar(',');
+                ft_putchar(' ');
+            }
+            string[2]++; 
+        }
+        
+        while(string[1] <= '9')
+        {
+            ft_putchar(string[0]);
+            ft_putchar(string[1]);
+            ft_putchar(string[2]);
+            if (string[0] != '7' || string[1] != '8' || string[2] != '9') {
+                ft_putchar(',');
+                ft_putchar(' ');
+            }
+            string[1]++;
+        }
+        
+    
+}
+
+int main(void) {
+    // ft_print_comb();
+    ft_print_dozens();
+    return (0);
+}
