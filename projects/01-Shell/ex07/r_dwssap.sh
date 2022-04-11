4743dafd2475f2ab5de7a200e34fe8563b5d1669
Gabriel Schiavo <gschiavo@student.42sp.org.br>
2022-04-11 23:49:36 +0200

    Add Shell01/ex07 fixed solution

--- projects/01-Shell/ex07/r_dwssap.sh
+++ projects/01-Shell/ex07/r_dwssap.sh
@@ -1,4 +1,4 @@
 #! /usr/bin bash
 
-cat /etc/passwd | awk -F':' '{print $5}'
+cat /etc/passwd | grep -v "^#" | awk 'NR%2==0' | cut -d ':' -f 1 | rev | sort -r | head -n $FT_LINE2 | tail -n $(($FT_LINE2-$FT_LINE1+1)) | tr "\n" ", " | sed s/,$/./ | tr -d "\n"
 
