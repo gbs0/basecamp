0b68d96649939fb74ecab0934f55ab1434631567
Gabriel Schiavo <gschiavo@student.42sp.org.br>
2022-04-12 21:04:03 +0200

    Add 5th attempt on Shell01

--- projects/01-Shell/ex07/r_dwssap.sh
+++ projects/01-Shell/ex07/r_dwssap.sh
@@ -1,4 +1,4 @@
 #! /usr/bin bash
 
-cat /etc/passwd | grep -v "^#" | awk 'NR%2==0' | cut -d ':' -f 1 | rev | sort -r | head -n $FT_LINE2 | tail -n $(($FT_LINE2-$FT_LINE1+1)) | tr "\n" ", " | sed s/,$/./ | tr -d "\n"
+cat /etc/passwd | grep --invert-match "^#" | sed -n "p;n" | cut -d : -f 1 | rev | sort -r | awk "NR>=$FT_LINE1 && NR<=$FT_LINE2" | tr "\n" "," | sed 's/,/, /g' | sed 's/, $/./'
 
