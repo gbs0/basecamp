38c79a90a408f3016b4f06b43159de499ed3f7e9
Gabriel Schiavo <gschiavo@student.42sp.org.br>
2022-04-13 16:20:58 +0200

    Norma style on C02/ex02

--- projects/C02/ex02/ft_str_is_alpha.c
+++ projects/C02/ex02/ft_str_is_alpha.c
@@ -1,20 +1,29 @@
-// Header
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_str_is_alpha.c                                  :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gschiavo <gschiavo@student.42sp.org.br>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/04/13 15:26:56 by gschiavo          #+#    #+#             */
+/*   Updated: 2022/04/13 16:20:24 by gschiavo         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
 #include <stdio.h>
 
-int ft_str_is_alpha(char *str)
+int	ft_str_is_alpha(char *str)
 {
-    int i;
-    i = 0;
+	int	i;
 
-    if (str[i] == '\0')
-        return (1);
-
-    while (str[i] != '\0')
-    {
-        if ((str[i] < 'a' || str[i] > 'z') && (str[i] < 'A' || str[i] > 'Z'))
-            return (0);
-        i++;
-    }
-    return (1);
+	i = 0;
+	if (str[i] == '\0')
+		return (1);
+	while (str[i] != '\0')
+	{
+		if ((str[i] < 'a' || str[i] > 'z') && (str[i] < 'A' || str[i] > 'Z'))
+			return (0);
+		i++;
+	}
+	return (1);
 }
