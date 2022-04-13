6bebafc8465ed025eb9889a14ded97d5337e1cab
Gabriel Schiavo <gschiavo@student.42sp.org.br>
2022-04-13 15:20:56 +0200

    Norma style on C02/ex01

--- projects/C02/ex01/ft_strncpy.c
+++ projects/C02/ex01/ft_strncpy.c
@@ -1,20 +1,28 @@
-// Header
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strncpy.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gschiavo <gschiavo@student.42sp.org.br>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/04/13 15:13:20 by gschiavo          #+#    #+#             */
+/*   Updated: 2022/04/13 15:19:31 by gschiavo         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
 #include <unistd.h>
 
-char    *ft_strncpy(char *dest, char *src, unsigned int n)
+char	*ft_strncpy(char *dest, char *src, unsigned int n)
 {
-    int     i;
-    i = 0;
-    
-    if (n == 0)
-        return (dest);
-    
-    while(i < n)
-    {
-        dest[i] = src[i];
-        i++;
-    }
-    
-    return(dest);
+	int		i;
+
+	i = 0;
+	if (n == 0)
+		return (dest);
+	while (i < n)
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	return (dest);
 }
