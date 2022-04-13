f2d30ded12c4630214cf3cc4d3a9345b2b6510f3
Gabriel Schiavo <gschiavo@student.42sp.org.br>
2022-04-13 14:25:33 +0200

    Norma style on C02/ex00

--- projects/C02/ex00/ft_strcpy.c
+++ projects/C02/ex00/ft_strcpy.c
@@ -3,26 +3,24 @@
 /*                                                        :::      ::::::::   */
 /*   ft_strcpy.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: gbs0 <gbs0@student.42.fr>                  +#+  +:+       +#+        */
+/*   By: gschiavo <gschiavo@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/12 13:15:39 by gschiavo          #+#    #+#             */
-/*   Updated: 2022/04/12 15:05:57 by gbs0             ###   ########.fr       */
+/*   Updated: 2022/04/13 14:24:58 by gschiavo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
 
-char *ft_strcpy(char *dest, char *src)
+char	*ft_strcpy(char *dest, char *src)
 {
-    int     i;
-    i = 0;
-    
-    while(i <= (sizeof(src) + 1))
-    {
-        dest[i] = src[i];
-        i++;
-    }
-    
-    return(dest);
-}
+	int		i;
 
+	i = 0;
+	while (i <= (sizeof(src) + 1))
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	return (dest);
+}
