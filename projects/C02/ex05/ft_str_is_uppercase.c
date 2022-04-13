a69a2106520304149be7c7485a70001dfb56f6d2
Gabriel Schiavo <gschiavo@student.42sp.org.br>
2022-04-13 18:53:22 +0200

    Fix norma in C02/ex05 exercise

--- projects/C02/ex05/ft_str_is_uppercase.c
+++ projects/C02/ex05/ft_str_is_uppercase.c
@@ -1,25 +1,34 @@
-// Header 
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_str_is_uppercase.c                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gschiavo <gschiavo@student.42sp.org.br>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/04/13 18:50:01 by gschiavo          #+#    #+#             */
+/*   Updated: 2022/04/13 18:52:33 by gschiavo         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
 #include <unistd.h>
 
-int ft_str_is_uppercase(char *str)
+int	ft_str_is_uppercase(char *str)
 {
-    int i;
-    
-    i = 0;
-    
-    if (*str == '\0')
-        return (1);
-    
-    while(*str != '\0')
-    {
-        if (*str >= 'A' && *str <= 'Z')
-        {
-            str++;
-        } else {
-            return (0);
-        }
-    }
-    
-    return(1);
+	int		i;
+
+	i = 0;
+	if (*str == '\0')
+		return (1);
+	while (*str != '\0')
+	{
+		if (*str >= 'A' && *str <= 'Z')
+		{
+			str++;
+		}
+		else
+		{
+			return (0);
+		}
+	}
+	return (1);
 }
