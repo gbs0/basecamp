67eb4e8db330349fb1f1af577713a13ca70b8fa1
Gabriel Schiavo <gschiavo@student.42sp.org.br>
2022-04-13 17:20:01 +0200

    WIP in C02/ex03 norma

--- projects/C02/ex03/ft_str_is_numeric.c
+++ projects/C02/ex03/ft_str_is_numeric.c
@@ -1,23 +1,32 @@
-// Header
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_str_is_numeric.c                                :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gschiavo <gschiavo@student.42sp.org.br>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/04/13 16:21:41 by gschiavo          #+#    #+#             */
+/*   Updated: 2022/04/13 16:29:06 by gschiavo         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
 #include <unistd.h>
 
-int ft_str_is_numeric(char *str)
+int	ft_str_is_numeric(char *str)
 {
-    int i;
+    int		i;
 
     i = 0;
 
     if (str[i] == '\0')
-        return (1);
+	return (1);
     
     while (str[i] != '\0')
     {
-        if (str[i] < '0' || str[i] > '9')
-            return (0);
-        i++;
+	if (str[i] < '0' || str[i] > '9')
+	    return (0);
+	i++;
     }
 
-    return (1);
-    
+    return	(1);
 }
