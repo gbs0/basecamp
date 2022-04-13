be8da61bfb797adb844edc5fc9eb4d0b7819d1d6
Gabriel Schiavo <gschiavo@student.42sp.org.br>
2022-04-13 18:30:48 +0200

    Fix norma in C02/ex04 exercise

--- projects/C02/ex04/ft_str_is_lowercase.c
+++ projects/C02/ex04/ft_str_is_lowercase.c
@@ -1,25 +1,34 @@
-// Header
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_str_is_lowercase.c                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gschiavo <gschiavo@student.42sp.org.br>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/04/13 18:23:39 by gschiavo          #+#    #+#             */
+/*   Updated: 2022/04/13 18:29:58 by gschiavo         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
 #include <unistd.h>
 
-int ft_str_is_lowercase(char *str) 
+int	ft_str_is_lowercase(char *str)
 {
-    int i;
-    
-    i = 0;
-    
-    if (*str == '\0')
-        return (1);
+	int		i;
 
-    while(*str != '\0')
-    {
-        if (*str >= 'a' && *str <= 'z')
-        {
-            str++;
-        } else {
-            return (0);
-        }
-    }
-
-    return(1);
+	i = 0;
+	if (*str == '\0')
+		return (1);
+	while (*str != '\0')
+	{
+		if (*str >= 'a' && *str <= 'z')
+		{
+			str++;
+		}
+		else
+		{
+			return (0);
+		}
+	}
+	return (1);
 }
