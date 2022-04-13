88ffad2d5a25d2c47d71c29ab617553956f0e3a9
Gabriel Schiavo <gschiavo@student.42sp.org.br>
2022-04-13 18:23:03 +0200

    Fix norma in C02/ex03

--- projects/C02/ex03/ft_str_is_numeric.c
+++ projects/C02/ex03/ft_str_is_numeric.c
@@ -6,7 +6,7 @@
 /*   By: gschiavo <gschiavo@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/13 16:21:41 by gschiavo          #+#    #+#             */
-/*   Updated: 2022/04/13 16:29:06 by gschiavo         ###   ########.fr       */
+/*   Updated: 2022/04/13 18:19:39 by gschiavo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,19 +14,16 @@
 
 int	ft_str_is_numeric(char *str)
 {
-    int		i;
+	int		i;
 
-    i = 0;
-
-    if (str[i] == '\0')
+	i = 0;
+	if (str[i] == '\0')
+		return (1);
+	while (str[i] != '\0')
+	{
+		if (str[i] < '0' || str[i] > '9')
+			return (0);
+		i++;
+	}
 	return (1);
-    
-    while (str[i] != '\0')
-    {
-	if (str[i] < '0' || str[i] > '9')
-	    return (0);
-	i++;
-    }
-
-    return	(1);
 }
