ee89682b6b0c6ba63d8ee3dea27d912c4e50fcf7
Gabriel Schiavo <gabrielschiavo0@gmail.com>
2022-04-14 08:43:05 -0300

    WIP in C03/ exercices

---
+++ projects/C03/ex00/main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int ft_strcmp(char *s1, char *s2);
+
+int	main(void)
+{
+	char	*original = "abcde";
+	char	*diff = "abcd";
+	
+	printf("%i\n", ft_strcmp(a, b));
+    printf("ft_strcmp\t= %d\n strcmp:\t= %d", ft_strcmp(original, diff), strcmp(original, diff));
+}
