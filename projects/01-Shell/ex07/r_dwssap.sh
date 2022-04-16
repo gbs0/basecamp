b07309cecada14f90a86b83a3a9e69ff320dcd58
Gabriel Schiavo <gschiavo@student.42sp.org.br>
2022-04-16 15:21:23 +0200

    Fix duplicate in Shell01/ex07

--- projects/01-Shell/ex07/r_dwssap.sh
+++ projects/01-Shell/ex07/r_dwssap.sh
@@ -1,4 +1,3 @@
 #! /usr/bin bash
 
-cat /etc/passwd | grep -v "#" | sed -n '0~2p' | sort | cut -d : -f1 | rev | sort -r | sed -n "$FT_LINE1,$FT_LINE2 p" | tr "\n" "," | tr "\n" "," | sed 's/,/, /g' | sed 's/, $/./'
-
+cat /etc/passwd | grep -v "#" | sed -n '0~2p' | sort | cut -d : -f1 | rev | sort -r | sed -n "$FT_LINE1,$FT_LINE2 p" | tr "\n" "," | sed 's/,/, /g' | sed 's/, $/./'
