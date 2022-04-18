6d7b951dc516c7e34ce7aa92876b83beadb280cb
Gabriel Schiavo <gabrielschiavo0@gmail.com>
2022-04-18 08:03:23 -0300

    Fix C02/ex00 function

--- projects/C02/ex00/ft_strcpy.c
+++ projects/C02/ex00/ft_strcpy.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_strcpy.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: gschiavo <gschiavo@student.42sp.org.br>    +#+  +:+       +#+        */
+/*   By: gschiavo <gschiavo@42student.org.br>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/12 13:15:39 by gschiavo          #+#    #+#             */
-/*   Updated: 2022/04/13 14:24:58 by gschiavo         ###   ########.fr       */
+/*   Updated: 2022/04/18 08:01:39 by gschiavo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,10 +17,39 @@ char	*ft_strcpy(char *dest, char *src)
 	unsigned int		i;
 
 	i = 0;
-	while (i <= (sizeof(src) + 1))
+	while (src[i] != '\0')
 	{
 		dest[i] = src[i];
 		i++;
 	}
+	dest[i] = '\0';
 	return (dest);
 }
+
+// ------------------------- TEST -------------------------
+// #include <unistd.h>
+// #include <stdio.h>
+// #include <stdlib.h>
+// #include <string.h>
+// char	*ft_strcpy(char *dest, char *src);
+// void ft_strcpy_test(char *ft_res, char *str_res)
+// {
+// 	if (ft_res != str_res)
+// 		printf("> KO, expected: %s, got: %s\n", ft_res, str_res);
+// 	else
+// 		printf("> OK, result: %s, expected: %s\n", ft_res, str_res);
+// }
+// int	main(void)
+// {
+// 	char *src;
+// 	char *dest;
+// 	char *result;
+// 	char *expected;
+// 	src = calloc(11, sizeof(char));
+// 	src = "0123456789";
+// 	dest = calloc(11, sizeof(char));
+// 	result = ft_strcpy(dest, src);
+// 	expected = strcpy(dest, src);
+// 	ft_strcpy_test(result, expected);
+//     return (0);
+// }
--- projects/C02/ex00/main.c
+++ projects/C02/ex00/main.c
@@ -1,22 +1,31 @@
 #include <unistd.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 char	*ft_strcpy(char *dest, char *src);
 
+
+void ft_strcpy_test(char *ft_res, char *str_res)
+{
+	if (ft_res != str_res)
+		printf("> KO, expected: %s, got: %s\n", ft_res, str_res);
+	else
+		printf("> OK, result: %s, expected: %s\n", ft_res, str_res);
+}
 int	main(void)
 {
 	char *src;
 	char *dest;
 	char *result;
+	char *expected;
 
 	src = calloc(11, sizeof(char));
 	src = "0123456789";
 	dest = calloc(11, sizeof(char));
-	result = ft_strcpy(dest, src);	
-	printf("src: %s\n", src);
-	printf("dest: %s\n", dest);
-	printf("result: %s\n", result);
+	result = ft_strcpy(dest, src);
+	expected = strcpy(dest, src);
+	ft_strcpy_test(result, expected);
     return (0);
 
 }
