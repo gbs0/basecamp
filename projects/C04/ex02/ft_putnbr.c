b47ef9f1bdbc222b322c3260d9b300a3f13ae74f
Gabriel Schiavo <gabrielschiavo0@gmail.com>
2022-04-18 17:08:10 -0300

    Finish C04/ex02 exercise

---
+++ projects/C04/ex02/ft_putnbr.c
@@ -0,0 +1,56 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_putnbr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gschiavo <gschiavo@42student.org.br>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/04/18 16:37:58 by gschiavo          #+#    #+#             */
+/*   Updated: 2022/04/18 17:07:11 by gschiavo         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	ft_putchar(char s)
+{
+	write(1, &s, 1);
+}
+
+void	ft_putstr(char *c)
+{
+	int		i;
+
+	i = 0;
+	while (c[i] != '\0')
+	{
+		ft_putchar(*c);
+		i++;
+	}
+}
+
+void	ft_putnbr(int nb)
+{
+	if (nb == -2147483648)
+	{
+		ft_putchar('-');
+		write(1, "2147483648", 10);
+		return ;
+	}
+	if (nb == 0)
+		ft_putchar(0);
+	if (nb <= 0)
+	{
+		ft_putchar('-');
+		nb = -nb;
+	}
+	if (nb > 9)
+	{
+		ft_putnbr(nb / 10);
+		ft_putchar(nb % 10 + '0');
+	}
+	else if (nb >= 2147483647)
+		write(1, "2147483648", 10);
+	else
+		ft_putchar(nb % 10 + '0');
+}
