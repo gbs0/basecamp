8bb8af8f597162bb325eda039e5aa5664467b1a7
Gabriel Schiavo <gabrielschiavo0@gmail.com>
2022-04-20 17:36:25 -0300

    Finish C05/ex02 exercise

---
+++ projects/C05/ex02/ft_iterative_power.c
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_iterative_power.c                               :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gschiavo <gschiavo@42student.org.br>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/04/20 15:57:54 by gschiavo          #+#    #+#             */
+/*   Updated: 2022/04/20 17:31:59 by gschiavo         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+int	ft_iterative_power(int nb, int power)
+{
+	int	i;
+	int	result;
+
+	i = 0;
+	result = 1;
+	if (power < 0)
+		return (0);
+	if (nb == 0 && power == 0)
+		return (1);
+	while (i < power)
+	{
+		result *= nb;
+		i++;
+	}
+	return (result);
+}
