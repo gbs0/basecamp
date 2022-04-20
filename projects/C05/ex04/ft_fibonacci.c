a49227e4ac5015574924afd8cb49fdae159686d1
Gabriel Schiavo <gabrielschiavo0@gmail.com>
2022-04-20 18:06:17 -0300

    Finish C05/ex04 exercise

---
+++ projects/C05/ex04/ft_fibonacci.c
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_fibonacci.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gschiavo <gschiavo@42student.org.br>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/04/20 17:47:05 by gschiavo          #+#    #+#             */
+/*   Updated: 2022/04/20 18:01:32 by gschiavo         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+int	ft_fibonacci(int index)
+{
+	if (index < 0)
+		return (-1);
+	if (index == 0)
+		return (0);
+	if (index == 1)
+		return (1);
+	if (index == 2)
+		return (1);
+	return (ft_fibonacci(index - 1) + ft_fibonacci(index - 2));
+}
