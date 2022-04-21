7e77928ad5bc4c088edcdd876e5484c7da5ef36b
Gabriel Schiavo <gabrielschiavo0@gmail.com>
2022-04-21 20:30:36 -0300

    Improve C05/ex05 comparison between int

--- projects/C05/ex05/ft_sqrt.c
+++ projects/C05/ex05/ft_sqrt.c
@@ -13,26 +13,17 @@
 int	ft_sqrt(int nb)
 {
 	int		factor;
-	int		result;
 
-	if (nb < 0)
-		return (0);
-	if (nb == 0)
-		return (0);
-	if (nb == 1)
-		return (1);
 	factor = 1;
-	result = factor;
 	if (nb > 0)
 	{
-		while (result <= nb)
+		while ((factor * factor) <= nb)
 		{
-			if (result == nb)
+			if ((factor * factor) == nb)
 				return (factor);
-			else if (result >= 46341)
+			else if (factor >= 46341)
 				return (0);
 			factor++;
-			result = factor * factor;
 		}
 	}
 	return (0);
