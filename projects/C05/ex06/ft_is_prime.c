2a5742ea9dc999e82b91b22588495aac9f029bb1
Gabriel Schiavo <gabrielschiavo0@gmail.com>
2022-04-21 15:30:41 -0300

    Finish C05/ex06 exercise

--- projects/C05/ex06/ft_is_prime.c
+++ projects/C05/ex06/ft_is_prime.c
@@ -1,18 +1,31 @@
-int     ft_is_prime(int nb)
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_is_prime.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gschiavo <gschiavo@42student.org.br>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/04/21 15:29:14 by gschiavo          #+#    #+#             */
+/*   Updated: 2022/04/21 15:30:28 by gschiavo         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+int	ft_is_prime(int nb)
 {
-    int i;
-    i = 2;
-    if (nb < 0)
-        return (0);
-    if (nb == 0 || nb == 1)
-        return (0);
-    if (nb < 2)
-        return (0);
-    while (i <= nb / 2)
-    {
-        if (nb % i == 0)
-            return (0);
-        i++;
-    }
-    return (1);
+	int	i;
+
+	i = 2;
+	if (nb < 0)
+		return (0);
+	if (nb == 0 || nb == 1)
+		return (0);
+	if (nb < 2)
+		return (0);
+	while (i <= nb / 2)
+	{
+		if (nb % i == 0)
+			return (0);
+		i++;
+	}
+	return (1);
 }
