bfa2267029408903f19016b1ad229972177707c1
Gabriel Schiavo <gabrielschiavo0@gmail.com>
2022-04-22 17:02:57 -0300

    Finish C06/ex00

---
+++ projects/C06/ex00/ft_print_program_name.c
@@ -0,0 +1,42 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_print_program_name.c                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gschiavo <gschiavo@student.42sp.org.br>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/04/22 16:54:22 by gschiavo          #+#    #+#             */
+/*   Updated: 2022/04/22 17:02:19 by gschiavo         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
+
+void	ft_putstr(char *str)
+{
+	int		i;
+
+	i = 0;
+	while (str[i] != '\0')
+	{
+		ft_putchar(str[i]);
+		i++;
+	}
+}
+
+int	main(int argc, char **argv)
+{
+	int		i;
+
+	i = 0;
+	if (i >= argc)
+		ft_putstr(argv[i]);
+	ft_putstr(argv[i]);
+	ft_putchar('\n');
+	return (0);
+}
