17f70e1e9e43a88865d8000b915dd1c5ecf03d57
Gabriel Schiavo <gabrielschiavo0@gmail.com>
2022-04-22 17:14:15 -0300

    Finish C06/ex01 exercise

---
+++ projects/C06/ex01/ft_print_params.c
@@ -0,0 +1,44 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_print_params.c                                  :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gschiavo <gschiavo@student.42sp.org.br>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/04/22 17:06:44 by gschiavo          #+#    #+#             */
+/*   Updated: 2022/04/22 17:12:46 by gschiavo         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
+
+void	ft_putstr(char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i] != '\0')
+	{
+		ft_putchar(str[i]);
+		i++;
+	}
+}
+
+int	main(int argc, char **argv)
+{
+	int	i;
+
+	i = 0;
+	while (i < argc - 1)
+	{
+		ft_putstr(argv[i + 1]);
+		ft_putchar('\n');
+		i++;
+	}
+	return (0);
+}
