6bd28cf36a223585e092c671e6b3413f541e7614
Gabriel Schiavo <gabrielschiavo0@gmail.com>
2022-05-27 15:18:26 -0300

    Add libft Header file

---
+++ projects/libft/include/libft.h
@@ -0,0 +1,31 @@
+#ifndef LIBFT_H
+# define LIBFT_H
+# include <string.h>
+# include <stdlib.h>
+# include <unistd.h>
+# include <limits.h>
+
+typedef unsigned char	t_uchar;
+typedef struct	s_list
+{
+	void			*content;
+	struct s_list	*next;
+}				t_list;
+
+/*
+** ==== memory management ====
+*/
+
+/*
+** ==== display ====
+*/
+void			ft_putchar(char c);
+void			ft_putstr(char *s);
+void			ft_putnbr(int n);
+void			ft_putchar_fd(char c, int fd);
+
+/*
+** ==== string manipulation ====
+*/
+
+#endif
