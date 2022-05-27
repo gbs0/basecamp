3e97083658746b25b5cae2c8c0a69fce923b386e
Gabriel Schiavo <gabrielschiavo0@gmail.com>
2022-05-27 17:03:28 -0300

    [DONE] - Libft - ft_putnbr_fd.c

---
+++ projects/libft/src/display/ft_putnbr_fd.c
@@ -0,0 +1,35 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_putnbr_fd.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gschiavo <gschiavo@42student.org.br>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/05/27 15:29:41 by gschiavo          #+#    #+#             */
+/*   Updated: 2022/05/27 17:02:34 by gschiavo         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	ft_putnbr_fd(int n, int fd)
+{
+	long	nb;
+
+	nb = n;
+	if (n < 0)
+	{
+		ft_putchar_fd('-', fd);
+		nb = -n;
+	}
+	if (nb < 10)
+	{
+		ft_putchar_fd(nb + '0', fd);
+		return ;
+	}
+	else
+	{
+		ft_putnbr_fd(nb / 10, fd);
+		ft_putnbr_fd(nb % 10, fd);
+	}
+}
