25238ae78c76b29c35c0dbae6a9eab23d72fdc16
Gabriel Schiavo <gabrielschiavo0@gmail.com>
2022-05-31 11:13:36 -0300

    [DONE] - Libft - memory/ft_memchr.c

--- projects/libft/src/memory/ft_memchr.c
+++ projects/libft/src/memory/ft_memchr.c
@@ -6,26 +6,26 @@
 /*   By: gschiavo <gschiavo@42student.org.br>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/30 18:26:19 by gschiavo          #+#    #+#             */
-/*   Updated: 2022/05/30 19:58:25 by gschiavo         ###   ########.fr       */
+/*   Updated: 2022/05/31 11:12:52 by gschiavo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void    *ft_memchr(const void *s, int c, size_t n)
+void	*ft_memchr(const void *s, int c, size_t n)
 {
-    size_t  i;
-    unsigned char *src_c;
+	size_t			i;
+	unsigned char	*src_c;
 
-    i = 0;
-    src_c = (unsigned char *)s;
-    if (n == 0)
-        return (0);
-    while (i < n)
-    {
-        if (src_c[i] == (unsigned char)c)
-            return ((void *)&src_c[i]);
-        i++;
-    }
-    return (0);
+	i = 0;
+	src_c = (unsigned char *)s;
+	if (n == 0)
+		return (0);
+	while (i < n)
+	{
+		if (src_c[i] == (unsigned char)c)
+			return ((void *)&src_c[i]);
+		i++;
+	}
+	return (0);
 }
