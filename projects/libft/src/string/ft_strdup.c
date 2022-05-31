398447a3dad31e10c9a759a3995da415b97e5785
Gabriel Schiavo <gabrielschiavo0@gmail.com>
2022-05-31 11:16:26 -0300

    [DONE] - Libft - string/ft_strdup.c

--- projects/libft/src/string/ft_strdup.c
+++ projects/libft/src/string/ft_strdup.c
@@ -6,17 +6,17 @@
 /*   By: gschiavo <gschiavo@42student.org.br>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/30 11:49:01 by gschiavo          #+#    #+#             */
-/*   Updated: 2022/05/30 11:54:26 by gschiavo         ###   ########.fr       */
+/*   Updated: 2022/05/31 11:15:28 by gschiavo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-char    *ft_strdup(const char *s1)
+char	*ft_strdup(const char *s1)
 {
-	size_t  i;
-	size_t  len;
-	char    *s2;
+	size_t	i;
+	size_t	len;
+	char	*s2;
 
 	i = 0;
 	len = ft_strlen(s1);
