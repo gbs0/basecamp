68c3b31e8fc0a15911e076bb34d6fb6fd234a668
Gabriel Schiavo <gabrielschiavo0@gmail.com>
2022-05-31 11:31:10 -0300

    [DONE] - Libft - memory/ft_memcmp.c

---
+++ projects/libft/src/memory/ft_memcmp.c
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memcmp.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gschiavo <gschiavo@42student.org.br>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/05/31 11:18:30 by gschiavo          #+#    #+#             */
+/*   Updated: 2022/05/31 11:30:02 by gschiavo         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+int	ft_memcmp(const void *s1, const void *s2, size_t n)
+{
+	size_t			i;
+	unsigned char	*str1;
+	unsigned char	*str2;
+
+	str1 = (unsigned char *)s1;
+	str2 = (unsigned char *)s2;
+	i = 0;
+	if (n == 0 || *str1 == *str2)
+		return (0);
+	while (n--)
+	{
+		if (str1[i] != str2[i])
+			return (str1[i] - str2[i]);
+		i++;
+	}
+	return (0);
+}
