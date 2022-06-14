084456b32ff39043e130190cd7ce95e748682925
Gabriel Schiavo <gabrielschiavo0@gmail.com>
2022-06-14 10:05:40 -0300

    [FIX] - Libft - memory/ft_calloc.c

--- projects/libft/src/memory/ft_calloc.c
+++ projects/libft/src/memory/ft_calloc.c
@@ -6,7 +6,7 @@
 /*   By: gschiavo <gschiavo@42student.org.br>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/30 11:10:24 by gschiavo          #+#    #+#             */
-/*   Updated: 2022/06/01 15:04:51 by gschiavo         ###   ########.fr       */
+/*   Updated: 2022/06/14 10:02:34 by gschiavo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,16 +14,15 @@
 
 void	*ft_calloc(size_t count, size_t size)
 {
-	void	*dest;
+	void	*pointer;
+	size_t	malloc_size;
 
-	if (count * size == 0)
-	{
-		size = 1;
-		count = 1;
-	}
-	dest = (void *)malloc(size * count);
-	if (!dest)
-		return (0);
-	ft_memset(dest, '\0', size * count);
-	return (dest);
+	malloc_size = count * size;
+	if (count != 0 && malloc_size / count != size)
+		return (NULL);
+	pointer = (void *)malloc(malloc_size);
+	if (pointer == NULL)
+		return (NULL);
+	ft_bzero(pointer, malloc_size);
+	return (pointer);
 }
