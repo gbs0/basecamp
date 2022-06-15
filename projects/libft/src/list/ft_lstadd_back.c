a2698e4c733ce63cbe2c889b3ecc5e1e81602e78
Gabriel Schiavo <gabrielschiavo0@gmail.com>
2022-06-15 14:28:13 -0300

    [DONE] - Libft - list/ft_lstadd_back.c

--- projects/libft/src/list/ft_lstadd_back.c
+++ projects/libft/src/list/ft_lstadd_back.c
@@ -6,24 +6,24 @@
 /*   By: gschiavo <gschiavo@42student.org.br>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/14 11:36:15 by gschiavo          #+#    #+#             */
-/*   Updated: 2022/06/14 15:35:10 by gschiavo         ###   ########.fr       */
+/*   Updated: 2022/06/15 14:27:55 by gschiavo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void    ft_lstadd_back(t_list **alst, t_list *new)
+void	ft_lstadd_back(t_list **alst, t_list *new)
 {
-    t_list *last;
+	t_list	*last;
 
-    last = *alst;
-    new->next = NULL;
-    if(!*alst)
-        *alst = new;
-    else
-    {
-        while(last->next)
-            last = last->next;
-        last->next = new;
-    }
+	last = *alst;
+	new->next = NULL;
+	if (!*alst)
+		*alst = new;
+	else
+	{
+		while (last->next)
+			last = last->next;
+		last->next = new;
+	}
 }
