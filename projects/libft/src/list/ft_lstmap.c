585d48b348260b85a36d16667d3fc0dec0c6b1e4
Gabriel Schiavo <gabrielschiavo0@gmail.com>
2022-06-16 22:24:50 -0300

    [FIX] - Libft - list/ft_lstmap.c

--- projects/libft/src/list/ft_lstmap.c
+++ projects/libft/src/list/ft_lstmap.c
@@ -6,7 +6,7 @@
 /*   By: gschiavo <gschiavo@42student.org.br>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/14 15:51:46 by gschiavo          #+#    #+#             */
-/*   Updated: 2022/06/16 22:18:16 by gschiavo         ###   ########.fr       */
+/*   Updated: 2022/06/16 22:24:17 by gschiavo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,15 +19,15 @@ t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*d)(void *))
 
 	if (!lst || !f)
 		return (NULL);
-	new = ft_lstnew((*f)(lst->content))
-		if (!new)
+	new = ft_lstnew((*f)(lst->content));
+	if (!new)
 		return (NULL);
 	head = new;
 	lst = lst->next;
 	while (lst)
 	{
-		new->next = ft_lstnew((*f)(lst->content))
-			if (!new->next)
+		new->next = ft_lstnew((*f)(lst->content));
+		if (!new->next)
 		{
 			ft_lstclear(&head, (*d));
 			return (NULL);
