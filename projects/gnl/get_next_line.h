638396b6ed944a145a35881a9500345c13b2ad65
Gabriel Schiavo <gabrielschiavo0@gmail.com>
2022-06-23 20:43:13 -0300

    [DONE] - gnl - Fix norma style for header file

--- projects/gnl/get_next_line.h
+++ projects/gnl/get_next_line.h
@@ -6,7 +6,7 @@
 /*   By: gschiavo <gschiavo@42student.org.br>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/23 19:19:49 by gschiavo          #+#    #+#             */
-/*   Updated: 2022/06/23 20:05:55 by gschiavo         ###   ########.fr       */
+/*   Updated: 2022/06/23 20:42:33 by gschiavo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,9 +16,9 @@
 # include <unistd.h>
 # define MAX_FD_SIZE 1024
 
-char    *get_next_line(int fd);
-char    *ft_strchr(char *str, int c);
-int     ft_strlen(char *str);
-char    *ft_alloc(size_t mem, size_t size);
-char    *ft_new_buffer(char *s1, char *s2);
+char	*get_next_line(int fd);
+char	*ft_strchr(char *str, int c);
+int		ft_strlen(char *str);
+char	*ft_alloc(size_t mem, size_t size);
+char	*ft_new_buffer(char *s1, char *s2);
 #endif
