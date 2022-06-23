3a8dab25707a9b410a3f6ddf6fe8a925ff725f22
Gabriel Schiavo <gabrielschiavo0@gmail.com>
2022-06-23 20:06:18 -0300

    Add functions on file header

---
+++ projects/gnl/get_next_line.h
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gschiavo <gschiavo@42student.org.br>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/06/23 19:19:49 by gschiavo          #+#    #+#             */
+/*   Updated: 2022/06/23 20:05:55 by gschiavo         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+# include <stdlib.h>
+# include <unistd.h>
+# define MAX_FD_SIZE 1024
+
+char    *get_next_line(int fd);
+char    *ft_strchr(char *str, int c);
+int     ft_strlen(char *str);
+char    *ft_alloc(size_t mem, size_t size);
+char    *ft_new_buffer(char *s1, char *s2);
+#endif
