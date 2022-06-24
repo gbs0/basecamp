2099925aa07f9a2d92b658add70a6f97ec84424b
Gabriel Schiavo <gabrielschiavo0@gmail.com>
2022-06-23 21:33:45 -0300

    [WIP] - gnl - get_next_line.c

---
+++ projects/gnl/get_next_line.c
@@ -0,0 +1,95 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gschiavo <gschiavo@42student.org.br>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/06/23 19:59:46 by gschiavo          #+#    #+#             */
+/*   Updated: 2022/06/23 21:33:01 by gschiavo         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_next_line.h"
+
+char	*offset(char *buffer)
+{
+	size_t	i;
+	size_t	shift;
+	char	*new_buffer;
+
+	i = -1;
+	shift = 0;
+	if (!buffer)
+		return (NULL);
+	if (!*buffer)
+	{
+		free(buffer);
+		buffer = NULL;
+		return (buffer);
+	}
+	while (buffer[shift] && buffer[shift] != '\n')
+		shift++;
+	new_buffer = malloc(ft_strlen(buffer) + 1);
+	if (!new_buffer)
+		return (NULL);
+	while (buffer[shift])
+		new_buffer[++i] = buffer[shift++];
+	new_buffer[++i] = '\0';
+	free(buffer);
+	buffer = NULL;
+	return (new_buffer);
+}
+
+char	*get_breakline(char *buffer)
+{
+	size_t	i;
+	size_t	offset;
+	char	*line;
+
+	i = 0;
+	offset = 0;
+	if (!buffer)
+		return (NULL);
+	if (!*buffer)
+		return (NULL);
+	while (buffer[offset] && buffer[offset] != '\n')
+		offset++;
+	line = malloc((offset + 2) * sizeof(char));
+	if (!line)
+		return (NULL);
+	while (i <= offset)
+	{
+		line[i] = buffer[i];
+		i++;
+	}
+	line[i] = '\0';
+	return (line);
+}
+
+char	*buffer_handler(char *buffer, int fd)
+{
+	char	*temp;
+	char	*switch_buf;
+	int		ref_bytes;
+
+	ref_bytes = 1;
+	temp = malloc((BUFFER_SIZE + 1) * sizeof(char));
+	if (!temp)
+		return (NULL);
+	while (!ft_strchr(buffer, '\n') && ref_bytes != 0)
+	{
+		ref_bytes = read(fd, temp, BUFFER_SIZE);
+		if (0 > ref_bytes)
+		{
+			free(temp);
+			return (NULL);
+		}
+		temp[ref_bytes] = '\0';
+		switch_buf = ft_buffer_join(buffer, temp);
+		free(buffer);
+		buffer = switch_buf;
+	}
+	free(temp);
+	return (buffer);
+}
