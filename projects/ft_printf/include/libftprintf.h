18300fb523564ca2e9e58def3c49e217bbb1ef94
Gabriel Schiavo <gabrielschiavo0@gmail.com>
2022-06-27 12:26:05 -0300

    Rename putnbr function for print unsigned

--- projects/ft_printf/include/libftprintf.h
+++ projects/ft_printf/include/libftprintf.h
@@ -6,7 +6,7 @@
 /*   By: gschiavo <gschiavo@42student.org.br>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/27 11:08:39 by gschiavo          #+#    #+#             */
-/*   Updated: 2022/06/27 11:40:18 by gschiavo         ###   ########.fr       */
+/*   Updated: 2022/06/27 11:50:44 by gschiavo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,7 +20,7 @@
 int		ft_putchar(char c);
 int		ft_putnbr(int nbr);
 int		ft_putstr(char *str);
-int		putnbr_u(unsigned int nbr);
+int		putnbr_unsigned(unsigned int nbr);
 int		put_pointer(void *nbr, char *base);
 int		put_hex(unsigned int nbr, char *base);
 int		ft_printf(const char	*format, ...);
