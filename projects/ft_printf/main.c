612db8b339260d27bd81d81f0a0e32dbf32f7558
Gabriel Schiavo <gabrielschiavo0@gmail.com>
2022-06-30 16:48:28 -0300

    Add one more test case

--- projects/ft_printf/main.c
+++ projects/ft_printf/main.c
@@ -6,6 +6,13 @@ int main(){
     int ret_orig = printf("%p", ptr);
     int ret_gschiavo = ft_printf("%p", ptr);
 
+    int n = 15;
+    void* ptr2 = &n;
+    int ret_orig_2 = printf("%p", ptr2);
+    int ret_gschiavo_2 = ft_printf("%p", ptr2);
+
+
     printf("\nret orig = %d\nret gschiavo = %d\n", ret_orig, ret_gschiavo);
+    printf("\nret orig = %d\nret gschiavo = %d\n", ret_orig_2, ret_gschiavo_2);
     return 0;
 }
