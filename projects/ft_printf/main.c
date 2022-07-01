c8d1a61cbb9ded4ef716c18f6f8f2c39e9b4fe00
Gabriel Schiavo <gabrielschiavo0@gmail.com>
2022-07-01 17:56:08 -0300

    Update test

--- projects/ft_printf/main.c
+++ projects/ft_printf/main.c
@@ -3,13 +3,16 @@
 
 int main(){
     void* ptr = 0;
-    int ret_orig = printf("%p", ptr);
-    int ret_gschiavo = ft_printf("%p", ptr);
+    int ret_orig = printf(" %p %p ", ptr, ptr);
+    int ret_gschiavo = ft_printf("%p %p ", ptr, ptr);
 
 	
-	printf("%p\n", ptr);
-	ft_printf("%p\n", ptr);
+	// printf("%p\n", ptr);
+	// ft_printf("%p\n", ptr);
+    
     printf("\nret orig = %d\nret gschiavo = %d\n", ret_orig, ret_gschiavo);
 	
+    // printf(" %p %p \n", (void *) 0, (void *) 0);
+    // ft_printf(" %p %p \n", (void *) 0, (void *) 0);
     return 0;
 }
