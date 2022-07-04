a53a6c58b7f54bd8517a2acbccc246841306a034
Gabriel Schiavo <gschiavo@student.42sp.org.br>
2022-07-05 00:03:46 +0200

    Working on tests

--- projects/ft_printf/main.c
+++ projects/ft_printf/main.c
@@ -3,16 +3,27 @@
 
 int main(){
     void* ptr = 0;
-    int ret_orig = printf(" %p %p ", ptr, ptr);
-    int ret_gschiavo = ft_printf("%p %p ", ptr, ptr);
 
-	
+	int a = 10;
+    void* ptr2 = &a;
+    // int ret_orig = printf(" %p %p ", ptr, ptr);
+    // int ret_gschiavo = ft_printf("%p %p ", ptr, ptr);
+
+	int ret_orig = printf(" %p ", ptr);
+	int ret_gschiavo = ft_printf(" %p ", ptr);
 	// printf("%p\n", ptr);
 	// ft_printf("%p\n", ptr);
     
     printf("\nret orig = %d\nret gschiavo = %d\n", ret_orig, ret_gschiavo);
+
+	// printf("\nret orig = %d\n", ret_orig);
+	// printf("\nret gschiavo = %d\n", ret_gschiavo);
 	
     // printf(" %p %p \n", (void *) 0, (void *) 0);
     // ft_printf(" %p %p \n", (void *) 0, (void *) 0);
+
+	// printf(" %p \n", (void *) 0);
+    // ft_printf(" %p \n", (void *) 0);
+	
     return 0;
 }
